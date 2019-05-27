a1026a9225abc8a87a7ceeb13242b32bcad09d5d
3a713214 <48340352+3a713214@users.noreply.github.com>
2019-05-28 05:25:27 +0800

    Add files via upload

---
+++ 末6.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(void)
+{
+	char *ptr;
+	char string []="Happy New Year";
+	int length =strlen(string);
+	ptr =(char*)malloc(sizeof(char)*(length+1));
+	ptr=string;
+	puts(ptr);
+	system("PAUSE");
+return 0;	
+}
+
